c8569b18081cbd9e435e83c46c8be1c4c5ab3e04
dvid <contacto@dvidsilva.com>
2015-09-20 22:25:47 -0700

    created example with map underneath

--- components/map/map.js
+++ components/map/map.js
@@ -7,7 +7,9 @@ angular.module('dod.dashboard.map', []).directive('map', function ($log) {
   };
   var template = [
     "<div class=\"dashboard-map\">",
-    " <div ng-repeat='point in map.points' class='point animated' ng-class='point.type' ng-style=\"{'top': map.longLocation(point.long), 'left': map.latLocation(point.lat)}\"></div>",
+    " <div ng-repeat='point in map.points' class='point animated' ng-class='point.type' ng-style=\"{'left': map.longLocation(point.long), 'top': map.latLocation(point.lat)}\">",
+    "<span>{{point.label}}</span>",
+    "</div>",
     "</div>"
   ];
   return {
@@ -16,28 +18,33 @@ angular.module('dod.dashboard.map', []).directive('map', function ($log) {
     link: function (scope, elem) {
       var grid = [];
       $log.info(elem);
-      for (var i = 0; i < Constants.latTotal; i++ ) {
-        grid.push("<div class='lat'>");
-        for (var j = 0; j < Constants.longTotal; j++ ) {
-          grid.push('<div class="long"></div>');
+      for (var i = 1; i <= Constants.latTotal; i++ ) {
+        grid.push("<div class='lat' lat='"+i+"'>");
+        for (var j = 1; j <= Constants.longTotal; j++ ) {
+          grid.push('<div class="long" long="'+j+'"></div>');
         }
         grid.push("</div>");
       }
-      elem.append(grid.join(""));
+      elem.prepend(grid.join(""));
     },
     controllerAs: 'map',
     controller: function () {
       var map = this;
       map.longLocation = function (point) {
+        point = point + 180;
         return (point * Constants.longMultiplier) + "px";
       };
       map.latLocation = function (point) {
+        point = 90 - point;
+        $log.info(point);
         return (point * Constants.latMultiplier) + "px";
       };
       map.points = [
-        {lat: 100, long: 100, type: 'md'},
-        {lat: 90, long: 10, type: 'md'},
-        {lat: 300, long: 100, type: 'md'},
+        {lat: 37.7833, long: -122.4167, type: 'md', label: 'SF'},
+        {lat: 40.7127, long: -74.0059, type: 'md', label: 'New York'},
+        {lat: 35.6833, long: 139.6833, type: 'md', label: 'Tokio'},
+        {lat: 4.5981, long: -74.0758, type: 'md', label: 'Bogota'},
+        {lat: 0.2333, long: -78.5167, type: 'md', label: 'Quito'},
       ];
       return map;
     }
@@ -47,4 +54,4 @@ angular.module('dod.dashboard.map', []).directive('map', function ($log) {
 /*
  longitude: vertical : 360 lines
  latitude: horizontal : 180 lines
- * */
+ */
